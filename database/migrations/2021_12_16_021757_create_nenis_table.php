c5fb36ae61379d629d96f402fab4cb23478b8832
caprun <26richardr@gmail.com>
2021-12-15 20:25:24 -0600

    migrations

---
+++ database/migrations/2021_12_16_021757_create_nenis_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNenisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nenis', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('image');
+            $table->string('main_activity');
+            $table->date('neni_day');
+
+
+            $table->unsignedBigInteger('chem_type_id');
+            $table->foreign('chem_type_id')->references('id')->on('chems_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nenis');
+    }
+}
