032b60ba6580daa22ca671017d6c689aa979386e
caprun <26richardr@gmail.com>
2021-12-15 22:13:50 -0600

    seeders completed

--- database/seeders/ChemsSeeder.php
+++ database/seeders/ChemsSeeder.php
@@ -18,5 +18,25 @@ class ChemsSeeder extends Seeder
             'name' => 'Run Lomito',
             'description' => 'Es un run muy lomito',
         ]);
+        ChemsType::create([
+            'name' => 'God Lomito',
+            'description' => 'Chems es mi pastor nada me faltara',
+        ]);
+        ChemsType::create([
+            'name' => 'Markito Lomito Bomnito',
+            'description' => 'EL chems mas beio, si lo ves en persona te vuelves gei',
+        ]);
+        ChemsType::create([
+            'name' => 'Ruffle lomito',
+            'description' => 'Sin su ruffle vale queso',
+        ]);
+        ChemsType::create([
+            'name' => 'Chems con copia lomito',
+            'description' => 'Chems con copia lomito',
+        ]);
+        ChemsType::create([
+            'name' => 'Mama chems',
+            'description' => 'Mama de las nenis, amada por todo chems',
+        ]);
     }
 }
--- database/seeders/NeniSeeder.php
+++ database/seeders/NeniSeeder.php
@@ -21,12 +21,40 @@ class NeniSeeder extends Seeder
             'neni_day' => '2020-10-10',
             'chems_type_id' => 1
         ]);
-//        Neni::create([
-//            'name' => 'Chems Lomito',
-//            'image' => 'lomito.png',
-//            'main_activity' => 'Llevar a las nenis al lomimpo',
-//            'neni_day' => '2020-10-10',
-//            'chems_type_id' => 2
-//        ]);
+        Neni::create([
+            'name' => 'Chems Lomito',
+            'image' => 'lomito.png',
+            'main_activity' => 'Llevar a las nenis al lomimpo',
+            'neni_day' => '2020-10-10',
+            'chems_type_id' => 2
+        ]);
+        Neni::create([
+            'name' => 'Markitos Lomito',
+            'image' => 'markitos.png',
+            'main_activity' => 'Ser hermoso, si se vuelve gei run run lo aparta',
+            'neni_day' => '2020-10-10',
+            'chems_type_id' => 3
+        ]);
+        Neni::create([
+            'name' => 'Oskr',
+            'image' => 'markitos.png',
+            'main_activity' => 'Amar a su ruffle',
+            'neni_day' => '2020-10-10',
+            'chems_type_id' => 4
+        ]);
+        Neni::create([
+            'name' => 'Brandon lomito',
+            'image' => 'concopia.png',
+            'main_activity' => 'CC significa con copia',
+            'neni_day' => '2020-10-10',
+            'chems_type_id' => 5
+        ]);
+        Neni::create([
+            'name' => 'Wendy lomita',
+            'image' => 'concopia.png',
+            'main_activity' => 'CC significa con copia',
+            'neni_day' => '2021-12-10',
+            'chems_type_id' => 6
+        ]);
     }
 }
